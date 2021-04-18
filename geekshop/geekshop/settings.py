cfd2d646b4ff517f17c10e619e022727911e96b4
gbmikhail <vas1024@mail.ru>
2021-04-18 14:54:37 +0300

    server settings

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@%%gxo=5$@#zmz20gk682o0@lr2zkc87&$l$enkjdvznl^j9k8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -89,9 +89,15 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'geekshop',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        # 'PASSWORD': 'geekbrains'
     }
 }
 
